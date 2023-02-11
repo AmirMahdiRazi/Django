98bd7d9bf8c40e8e4f5015444b9df040da2cada2
Amir Mahdi Razi <raziamirmahdi@gmail.com>
2023-02-11 21:21:14 +0330

    season 5 work with model, database, admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+# @admin.regiter(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    # fields = ('title',)
+    list_display = ('title', 'counted_views', 'status', 'published_date')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    # ordering = ['-created_date']
+
+
+admin.site.register(Post, PostAdmin)
