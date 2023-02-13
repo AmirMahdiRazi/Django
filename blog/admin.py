1fa70b5f23b1dc8b41b99f1a8ff0e8b7dbc42328
Amir Mahdi Razi <raziamirmahdi@gmail.com>
2023-02-13 23:15:31 +0330

    Additional topics of the Database and View

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 # Register your models here.
 
 # @admin.regiter(Post)
@@ -9,10 +9,12 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
     # fields = ('title',)
-    list_display = ('title', 'counted_views', 'status', 'published_date')
-    list_filter = ('status',)
+    list_display = ('title', 'author', 'counted_views',
+                    'status', 'published_date')
+    list_filter = ('status', 'author')
     search_fields = ['title', 'content']
     # ordering = ['-created_date']
 
 
+admin.site.register(Category)
 admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+import asyncio
 
 
 def blog_view(request):
-    return render(request, 'blogs/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blogs/blog-home.html', context)
 
 
-def blog_single(request):
-    return render(request, 'blogs/blog-single.html')
+def blog_single(request, pid):
+    # Way 1
+    # post = get_object_or_404(Post, pk=pid,status=1)
+    # Way 2
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(posts, pk=pid, status=1)
+    # post = Post.objects.filter(status=1, id=1)
+    instance = Post.objects.values_list('id', 'title')
+    posts_list = list(instance)
+    index = posts_list.index((post.id, post.title))
+    prev_post = None
+    next_post = None
+    if (index == len(posts_list) - 1):
+        prev_post = posts[index-1]
+    elif (index == 0):
+        next_post = posts[index+1]
+    else:
+        next_post = posts[index+1]
+        prev_post = posts[index-1]
+    print(index, prev_post, next_post)
+    context = {'post': post, 'next_post': next_post, 'prev_post': prev_post}
+    # context = {'post': post}
+    return render(request, 'blogs/blog-single.html', context)
